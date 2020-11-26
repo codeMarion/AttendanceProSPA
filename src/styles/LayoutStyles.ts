9a12aac2960f645452f49955076cce4826f43e98
Marion Laanemae <ml553@sussex.ac.uk>
2020-11-26 17:45:02 +0000

    Added Layout and Topbar styles

---
+++ src/styles/LayoutStyles.ts
@@ -0,0 +1,48 @@
+import { createStyles, Theme } from "@material-ui/core";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import DashboardConfig from "../config/DashboardConfig";
+
+const LayoutStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: DashboardConfig.DRAWER_WIDTH,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${DashboardConfig.DRAWER_WIDTH}px)`,
+        marginLeft: DashboardConfig.DRAWER_WIDTH,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+      },
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      backgroundColor: theme.palette.primary.main,
+      width: DashboardConfig.DRAWER_WIDTH,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+    logoSection: {
+      backgroundColor: theme.palette.primary.main,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      cursor: 'pointer'
+    }
+  }),
+);
+
+export default LayoutStyles;
