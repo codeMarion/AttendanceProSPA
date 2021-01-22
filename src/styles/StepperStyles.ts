b3e848589170d39b973798966206a2de3f873811
Marion Laanemae <ml553@sussex.ac.uk>
2021-01-22 13:59:36 +0000

    Created FIle uploading form with stepper

---
+++ src/styles/StepperStyles.ts
@@ -0,0 +1,18 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+
+const StepperStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+    },
+    button: {
+      marginRight: theme.spacing(1),
+    },
+    instructions: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+    },
+  }),
+);
+
+export default StepperStyles;
--- src/views/Upload.tsx
+++ src/views/Upload.tsx
@@ -1,11 +1,103 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import { Stepper, Step, StepLabel, Button, Typography } from "@material-ui/core";
+import StepperStyles from "../styles/StepperStyles";
+import FileUpload from "../components/FileUpload";
+import UploadedDataCheck from "../components/UploadedDataCheck";
+import { UploadContext } from "../context/UploadContext";
+import { useHistory } from "react-router-dom";
+import { useSnackbar } from "notistack";
+import FileController from "../api/FileController";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const Upload = () => {
-    return(
-        <>
-            Upload
-        </>
-    )
+function getStepContent(step: number) {
+  switch (step) {
+    case 0:
+      return "Upload your data";
+    case 1:
+      return "Check the data";
+    default:
+      return "Unknown step";
+  }
+}
+function getSteps() {
+  return ["Upload your data", "Check the data"];
 }
 
-export default Upload
+const Upload = () => {
+  const history = useHistory();
+  const uploadContext = useContext(UploadContext)
+  const classes = StepperStyles();
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = getSteps();
+  const { enqueueSnackbar } = useSnackbar();
+  const fileController = new FileController();
+  const Auth0 = useAuth0();
+  
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    if(activeStep === 1){
+      Auth0.getAccessTokenSilently().then((accessToken) => {
+        fileController.uploadData(accessToken, uploadContext.uploadedData)
+      })
+      setActiveStep(0);
+      uploadContext.setUploadedData([]);
+      history.push("/");
+      enqueueSnackbar('Data uploaded successfully!', { variant: "success" });
+    }
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep}>
+        {steps.map(label => {
+          const stepProps: { completed?: boolean } = {};
+          const labelProps: { optional?: React.ReactNode } = {};
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      <div>
+          <div>
+            <Typography className={classes.instructions}>
+              {getStepContent(activeStep)}
+            </Typography>
+            {activeStep === 0 ? 
+                <FileUpload />
+            :
+            activeStep === 1 ?
+                <UploadedDataCheck />
+            :
+            <></>    
+            }
+            <div>
+              <Button
+                style={{display: activeStep === 1 ? 'inline' : 'none'}}
+                onClick={handleBack}
+                className={classes.button}
+                >
+                Back
+              </Button>
+              <Button
+                variant="contained"
+                disabled={uploadContext.uploadedData.length === 0}
+                color="primary"
+                onClick={handleNext}
+                className={classes.button}
+              >
+                {activeStep === steps.length - 1 ? "Confirm" : "Next"}
+              </Button>
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Upload;
