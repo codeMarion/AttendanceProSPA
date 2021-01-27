b886416bfc4cb0a5751b54cecd49dfe90bc08db3
Marion Laanemae <ml553@sussex.ac.uk>
2021-01-27 23:32:27 +0000

    Fixed Auth0 token on StudentPage query

--- src/components/StudentPage.tsx
+++ src/components/StudentPage.tsx
@@ -6,7 +6,6 @@ import StudentController from '../api/StudentController';
 import Student from '../models/Student'
 import { useAuth0 } from '@auth0/auth0-react';
 function StudentPage(props:any) {
-    const [accessToken, setAccessToken] = useState('');
     const Auth0 = useAuth0();
     const controller = new StudentController();
     const history = useHistory();
@@ -14,12 +13,14 @@ function StudentPage(props:any) {
     const [data, setData] = useState<Student>();
 
     useEffect(() => {
-        Auth0.getAccessTokenSilently().then(token => setAccessToken(token));
+        
         fetchData();
     },[])
 
     async function fetchData(){
-        const response = await controller.GetStudent(props.match.params.student, accessToken);
+        
+        const token = await Auth0.getAccessTokenSilently();
+        const response = await controller.GetStudent(props.match.params.student, token);
         if(response){
             setData(await response)
         }else if(response == false){
