258cb2868384bb59df1bf3c8a9e893ae99340d01
Marion Laanemae <ml553@sussex.ac.uk>
2021-02-10 01:09:07 +0000

    Minor fileupload styling fix

--- src/styles/FileUploadStyles.ts
+++ src/styles/FileUploadStyles.ts
@@ -4,6 +4,11 @@ const FileUploadStyles = makeStyles((theme: Theme) =>
   createStyles({
     table: {
       minWidth: 2000
+    },
+    dataCheck: {
+      height: 400, 
+      width: '100%', 
+      marginBottom: '1rem'
     }
   }),
 );
