e1c4f4debc03b932f688e6aff2cdb0d7da636a7c
Marion Laanemae <ml553@sussex.ac.uk>
2021-02-10 01:04:00 +0000

    Created a course controller to intergrate with backend services

---
+++ src/api/CourseController.ts
@@ -0,0 +1,49 @@
+import AuthConfig from "../config/AuthConfig";
+
+export default class StudentController 
+{
+    public async GetAllCourses(accessToken: string) {
+        const response = await fetch(`${AuthConfig.API_URL}/Course`,{
+            headers: {
+                "Authorization": `Bearer ${accessToken}`
+            }
+        });        
+        return response.ok ? await response.json() : false;
+    }
+
+    public async GetAbsenceData(body: string[], accessToken: string) {
+        const response = await fetch(`${AuthConfig.API_URL}/Course/absenceData`,{
+            method: 'POST',
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(body)
+        });        
+        return response.ok ? await response.json() : false;
+    }
+
+    public async GetAttendanceByPeriod(body: string[], accessToken: string) {
+        const response = await fetch(`${AuthConfig.API_URL}/Course/coursedata`,{
+            method: 'POST',
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(body)
+        });        
+        return response.ok ? await response.json() : false;
+    }
+
+    public async getAttendedByTeachingSessionsData(body: string[], accessToken: string) {
+        const response = await fetch(`${AuthConfig.API_URL}/Course/attendanceoverteaching`,{
+            method: 'POST',
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(body)
+        });        
+        return response.ok ? await response.json() : false;
+    }
+}
