738dbb8cbe8a7246c03448f6e7803289d5c1dd1a
Marion Laanemae <ml553@sussex.ac.uk>
2021-02-11 18:32:26 +0000

    Added filtering UI for students

--- src/views/Students.tsx
+++ src/views/Students.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Box, ButtonBase, Chip, Divider, Drawer, Grid, TextField, Typography } from "@material-ui/core";
 import StudentCard from "../components/StudentCard";
 import Pagination from '@material-ui/lab/Pagination';
 import StudentController from "../api/StudentController";
@@ -9,6 +9,10 @@ import StudentPage from "../models/StudentPage";
 import { useAuth0 } from "@auth0/auth0-react";
 import CircularProgress from '@material-ui/core/CircularProgress';
 import StudentsStyles from "../styles/StudentsStyles";
+import FilterIcon from "../assets/FilterIcon";
+import { Autocomplete } from "@material-ui/lab";
+import CourseResponse from "../models/CourseResponse";
+import CourseController from "../api/CourseController";
 
 const Students = () => {
   const classes = StudentsStyles();
@@ -16,43 +20,102 @@ const Students = () => {
   const [pages, setPages] = useState(0);
   const [currPage, setCurrPage] = useState(1);
   const [students, setStudents] = useState<StudentPage[]>();
+  const [filterDrawer, setFilterDrawer] = useState(false);
+  const [textInput, setTextInput] = useState('');
+  const [chipData, setChipData] = useState<CourseResponse[]>([]);
+  const [courses, setCourses] = useState<CourseResponse[]>([]);
   const controller = new StudentController();
+  const courseController = new CourseController();
   const appContext = useContext(AppContext);
 
   useEffect(() => {
     setPagesCount().then(() => GetStudents());
-  },[appContext.searchBarValue]);
-
+  },[appContext.searchBarValue, chipData]);
+  
   useEffect(() => {
+    GetCourses();
     GetStudents();
-  },[currPage])
+  },[currPage,chipData])
 
   const setPagesCount = async() => {
     const token = await Auth0.getAccessTokenSilently();
-    setPages(Math.ceil(await controller.GetStudentCount(appContext.searchBarValue, token)/12))
+    setPages(Math.ceil(await controller.GetStudentCount(appContext.searchBarValue, chipData, token)/12))
   }
   
   const GetStudents = async() => {
     const token = await Auth0.getAccessTokenSilently();
-    setStudents(await controller.GetStudentsPage(currPage, appContext.searchBarValue, token));
+    setStudents(await controller.GetStudentsPage(currPage, appContext.searchBarValue, chipData, token));
   }
+
+  const GetCourses = async () => {
+    const token = await Auth0.getAccessTokenSilently();
+    setCourses(await courseController.GetAllCourses(token));
+  };
+
+  const handleCourseFilter = (course: CourseResponse | null) => {
+    if(course){
+      setChipData(chipData => [...chipData, course]);      
+      setTextInput('');
+    }
+  };
+  
+  const handleChipDelete = (course: CourseResponse) => {
+    setChipData(chipData.filter(c => c.courseCode !== course.courseCode));
+  };
 
   return (
     <>
     {students ? 
-      <Grid container spacing={3}>
-        <Grid xs={12} className={classes.title}>
-          <Typography variant="h4">Students</Typography>
-        </Grid>
-        {students.map(student => (
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <StudentCard studentId={student.userId}/>
+      <>
+        <Grid container spacing={3}>
+          <Grid xs={12} className={classes.title}>
+            <Typography variant="h4">Students</Typography>
           </Grid>
-        ))}
-        <Grid xs={12} className={classes.pagination}>
-          <Pagination count={pages} variant="outlined" onChange={(event, value) => setCurrPage(value)}/>
+          <Grid xs={12}>
+              <ButtonBase onClick={() => setFilterDrawer(true)}>
+                <FilterIcon />
+              </ButtonBase>
+            </Grid>
+          {students.map(student => (
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <StudentCard studentId={student.userId}/>
+            </Grid>
+          ))}
+          <Grid xs={12} className={classes.pagination}>
+            <Pagination count={pages} variant="outlined" onChange={(event, value) => setCurrPage(value)}/>
+          </Grid>
         </Grid>
-      </Grid>
+        <Drawer variant="temporary" anchor={"right"} 
+        open={filterDrawer} 
+        classes={{paper: classes.drawerPaper}} 
+        onClose={() => setFilterDrawer(false)} 
+      >
+        <Grid >
+          <Grid xs={12} className={classes.filterStudentsTitle}>
+            <Typography variant="h5" align="center">Filter Students by Course</Typography>
+          </Grid>
+          <Grid container xs={12} className={classes.search}>
+            <Autocomplete
+              options={courses}
+              getOptionLabel={(course) => course.courseTitle}
+              className={classes.autoComplete}
+              onChange={(event, value, reason) => handleCourseFilter(value ?? null)}
+              renderInput={(params) => 
+                (
+                  <TextField {...params} label="Courses" variant="outlined" />
+                )}
+              inputValue={textInput}
+            />
+          </Grid>
+          <Divider />
+          <Box className={classes.chips}>
+            {chipData.map((course) => (
+              <Chip label={course.courseTitle} className={classes.chipStyle}  onDelete={() => handleChipDelete(course)} />
+              ))}
+          </Box>
+        </Grid>
+      </Drawer>
+      </>
     :
       <div className={classes.loading} color="secondary">
         <CircularProgress size={100} />
