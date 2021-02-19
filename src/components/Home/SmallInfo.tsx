80242ee4b7f8803f94e41ee0dc74b5f28c587c86
Marion Laanemae <ml553@sussex.ac.uk>
2021-02-19 03:35:01 +0000

    Reuseable component for small info created

---
+++ src/components/Home/SmallInfo.tsx
@@ -0,0 +1,32 @@
+import { Box, Card, CardContent, Typography } from '@material-ui/core'
+import React from 'react'
+
+interface SmallInfoProps {
+    title: string;
+    data: string;
+    imagesrc: string;
+}
+
+const SmallInfo = (props:SmallInfoProps) => {
+    return (
+        <Card>
+            <CardContent>
+                <Box style={{display: 'flex', justifyContent: 'space-between'}}>
+                    <Box>
+                        <Typography >
+                            {props.title}
+                        </Typography>
+                        <Typography variant="h4">
+                            {props.data}
+                        </Typography>
+                    </Box>
+                    <Box style={{display:'flex', justifyContent: 'center', alignItems: 'center',marginRight: '10%', padding: '5px 10px 5px 10px', backgroundColor: 'lightblue', borderRadius: '50%'}}>
+                        <img style={{height: '70%'}} src={props.imagesrc} />
+                    </Box>
+                </Box>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default SmallInfo
